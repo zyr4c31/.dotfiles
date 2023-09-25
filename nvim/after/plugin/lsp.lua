cad92aa4058f26f29cc3a7321a927ce5f79077c3
zyr4c31 <zyr4c31@gmail.com>
2023-09-25 17:15:51 +0800

    initial commit

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,14 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+---
+-- Replace these language servers
+-- with the ones you have installed in your system
+--
+lsp.preset('recommended')
+lsp.setup()
