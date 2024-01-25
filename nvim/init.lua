92b1042daf784deadfa15b2605206600fa61e1e9
zyr4c31 <zyr4c31@gmail.com>
2024-01-25 21:51:11 +0800

    chore: remove unneeded plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -257,7 +257,7 @@ require('lazy').setup({
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
   require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
@@ -576,11 +576,11 @@ local htmlFiletypes = { 'html', 'templ', 'jsx' }
 local servers = {
   -- clangd = {},
   gopls = {},
-  templ = { filetypes = { 'templ', 'go' } },
+  templ = {},
   -- pyright = {},
   -- rust_analyzer = {},
   tsserver = {},
-  htmx = { filetypes = htmlFiletypes },
+  htmx = { filetypes = "templ" },
   taplo = {},
   marksman = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs' } },
