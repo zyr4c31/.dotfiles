68ab05c00e38904addde109b6d32bef822082ff8
zyr4c31 <zyr4c31@gmail.com>
2024-08-22 11:14:09 +0800

    i3 update

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,12 +5,13 @@
 # cp -r i3status ~/.config/
 
 rm ~/.tmux.conf
-rm ~/.config/nvim
-rm ~/.config/alacritty
+
+rm -rf ~/.config/nvim
+rm -rf ~/.config/alacritty
+rm -rf ~/.config/i3
 
 ln -frs .tmux.conf ~/.tmux.conf
 ln -frs nvim ~/.config/
 ln -frs sqls ~/.config/
 ln -frs alacritty ~/.config/
-
 ln -frs i3 ~/.config/
