eb776e115c28a057f8ab95376809da7907e6f711
zyr4c31 <zyr4c31@gmail.com>
2025-06-04 08:52:38 +0800

    update refresh, i3config

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -3,13 +3,12 @@ monitors=`xrandr | grep "connected " | awk '{ print $1 }'`
 
 for monitor in $monitors; do
 	if [[ "$monitor" == "$primary" ]]; then
-		echo "primary $primary"
-		echo "monitor $monitor"
+		continue
 	fi
 
 	xrandr --output $monitor --off
 	xrandr --output $monitor --auto
-	xrandr --output $monitor --mode 1920x1080 --rate 144 --right-of eDP-1
+	xrandr --output $monitor --mode 1920x1080 --rate 144 --right-of $primary
 done
 
 xrandr
