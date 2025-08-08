d01e7e9320d1774a09b9ec6b185f9bdd8d6e45ef
zyr4c31 <zyr4c31@gmail.com>
2025-08-08 09:48:50 +0800

    update refresh

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -2,12 +2,13 @@ primary="eDP-1"
 monitors=`xrandr | grep "connected " | awk '{ print $1 }'`
 
 for monitor in $monitors; do
+	echo $monitor
 	if [[ "$monitor" == "$primary" ]]; then
 		continue
 	fi
 
 	xrandr --output $monitor --off
-	xrandr --output $monitor --auto
+	# xrandr --output $monitor --auto
 	xrandr --output $monitor --mode 1920x1080 --rate 144 --right-of $primary
 done
 
